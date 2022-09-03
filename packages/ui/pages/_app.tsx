a406be94840eea0147c054cf59801fe2ad28b17a
pisey <loempiseydec25@gmail.com>
2022-09-03 11:30:59 +0700

    add configuration with tialwind

--- packages/ui/pages/_app.tsx
+++ packages/ui/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {AppProps} from 'next/app'
 
-import '../styles/tailwind'
+import '../styles/tailwind.css'
 
 function MyApp({ Component, pageProps}: AppProps) {
     return <Component {...pageProps}/>
